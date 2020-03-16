cc4794d7d0da3d5d8114e057f09c1696d3d964f6
suttuang <tuangs@gmatc.matc.edu>
2020-03-16 15:25:49 -0500

    Completed Project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 //import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 import tasksRoute from './routes/tasks';
 
 // Initialize express
@@ -11,6 +12,11 @@ connectDatabase();
 
 // Middlewares
 app.use(express.json({ extended: false })); //handles JSON files
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
 // Set up a static folder for front-end
 app.use(express.static('public'));
