852553da8793c890a22c47705be1e8a5b294314d
suttuang <tuangs@gmatc.matc.edu>
2020-03-15 19:07:54 -0500

    Completed return tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,37 +5,37 @@ import Task from '../models/Task';
 const router = express.Router();
 
 // return tasks
-router.get('/', (req, res) => {
-    res.send("Return tasks for todo-list");
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    }catch(err) {
+        res.json({ message: err });
+    }
 });
 
-// add tasks
+// add a task
 router.post(
     '/', 
     [
         check('title', 'Please enter a task title!').not().isEmpty()
     ],
-    (req, res) => {
-        // console.log(req.body);
-        // res.send(req.body);
+    async (req, res) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.status(422).json({ errors : errors.array() })
         }else{
-            // return res.send(req.body);
-            //res.send(req.body.title);
             const task = new Task({
                 title: req.body.title
             });
 
-            // saving it to database
-            task.save()
-            .then( data => {
-                res.json(data);
-            })
-            .catch(err => {
+            try {
+                const savedTask = await task.save();
+                res.json(savedTask);
+                console.log('Added a new record to the database!')
+            }catch(err) {
                 res.json({ message: err });
-            });
+            }
         }
     }
 );
